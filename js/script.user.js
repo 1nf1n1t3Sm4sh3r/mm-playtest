6c299e4744bd659df85aacdd62d07e2324ac2a31
jmathew777 <infinitesmasher123@gmail.com>
2024-09-25 20:24:58 +0400

    Test Update

--- js/script.user.js
+++ js/script.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         The MegaMod (PLAYTEST) | Shell Shockers
-// @version      1.0
+// @version      1.1
 // @author       Infinite Smasher
 // @description  A bunch of useful mods, all brought together in one place
 // @icon         https://raw.githubusercontent.com/1nf1n1t3Sm4sh3r/mm-playtest/main/img/ico_mods.svg
-// @require      https://1nf1n1t3sm4sh3r.github.io/mm-playtest/js/megaModHtml.js?v=1.0
+// @require      https://1nf1n1t3sm4sh3r.github.io/mm-playtest/js/megaModHtml.js?v=1.1
 // @updateURL    https://1nf1n1t3sm4sh3r.github.io/mm-playtest/js/script.meta.js
 // @downloadURL  https://1nf1n1t3sm4sh3r.github.io/mm-playtest/js/script.user.js
 // @match        https://shellshock.io/
